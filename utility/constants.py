46ecf8e51c3fe0be1722218606203c5737d91693
zhili.zhou <zhili.zhou@ost.ch>
2024-11-13 13:52:17 +0100

    train data processed

--- utility/constants.py
+++
@@ -1,8 +0,0 @@
-DATA_DIR = './data'
-CACHE_FILE = './cachedir/flight_data_cache.joblib'
-
-ARR_FLIGHT_NAMES = [
-    "alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india",
-    "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo",
-    "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu"
-]
